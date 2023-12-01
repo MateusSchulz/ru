937d50115bd73bc274b93a67131edc0a2434e14b
Mateus Schulz <mateus.schulz.m@gmail.com>
2023-11-30 23:51:29 -0300

    feat: create menu

---
+++ src/app/menu/menu.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { MenuComponent } from './menu.component'
+
+@NgModule({
+  declarations: [MenuComponent],
+  imports: [CommonModule]
+})
+export class MenuModule {}
---
+++ src/app/menu/menu.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing'
+import { MenuService } from './menu.service'
+
+describe('MenuService', () => {
+  let service: MenuService
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(MenuService)
+  })
+
+  it('should be created', () => {
+    expect(service).toBeTruthy()
+  })
+})
